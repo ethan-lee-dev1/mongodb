00b9d4023ab945c3bf1091162734e743b5d9c180
Ethan Lee <elee@xpanxion.com>
2023-07-11 18:51:09 -0400

    save progress

--- client/components/CharacterCard.jsx
+++ client/components/CharacterCard.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import { FontAwesomeIcon as FAIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 
-const CharacterCard = ({
-  info, openModal
-}) => {
+const CharacterCard = ({ info, openModal }) => {
   const {
-    name, gender, species, birth_year, eye_color, skin_color,
-    hair_color, mass, height, homeworld, films = [], species_id, homeworld_id
+    name,
+    gender,
+    species,
+    birth_year,
+    eye_color,
+    skin_color,
+    hair_color,
+    mass,
+    height,
+    homeworld,
+    films = [],
+    species_id,
+    homeworld_id,
   } = info;
 
   const filmData = films.map((film, i) => (
     <li key={i} className="charFilm">
-      - {film.title} <span className="icon"><FAIcon icon={faQuestionCircle} size="xs" style={{color: 'steelBlue'}} onClick={e => openDetailsModal(e, 'film', film.id)} /></span>
+      - {film.title}{" "}
+      <span className="icon">
+        <FAIcon
+          icon={faQuestionCircle}
+          size="xs"
+          style={{ color: "steelBlue" }}
+          onClick={(e) => openDetailsModal(e, "film", film.id)}
+        />
+      </span>
     </li>
   ));
 
@@ -29,14 +46,34 @@ const CharacterCard = ({
       </div>
       <ul className="charDetailsList">
         <li className="charDetail">Gender: {gender}</li>
-        <li className="charDetail">Species: {species} <span className="icon"><FAIcon icon={faQuestionCircle} size="xs" style={{color: 'steelBlue'}} onClick={e => openDetailsModal(e, 'species', species_id)} /></span></li>
+        <li className="charDetail">
+          Species: {species}{" "}
+          <span className="icon">
+            <FAIcon
+              icon={faQuestionCircle}
+              size="xs"
+              style={{ color: "steelBlue" }}
+              onClick={(e) => openDetailsModal(e, "species", species_id)}
+            />
+          </span>
+        </li>
         <li className="charDetail">Birth Year: {birth_year}</li>
         <li className="charDetail">Eye Color: {eye_color}</li>
         <li className="charDetail">Skin Color: {skin_color}</li>
         <li className="charDetail">Hair Color: {hair_color}</li>
         <li className="charDetail">Mass: {mass}</li>
         <li className="charDetail">Height: {height}</li>
-        <li className="charDetail">Homeworld: {homeworld} <span className="icon"><FAIcon icon={faQuestionCircle} size="xs" style={{color: 'steelBlue'}} onClick={e => openDetailsModal(e, 'homeworld', homeworld_id)} /></span></li>
+        <li className="charDetail">
+          Homeworld: {homeworld}{" "}
+          <span className="icon">
+            <FAIcon
+              icon={faQuestionCircle}
+              size="xs"
+              style={{ color: "steelBlue" }}
+              onClick={(e) => openDetailsModal(e, "homeworld", homeworld_id)}
+            />
+          </span>
+        </li>
         <p className="charAddlDetail">Films:</p>
         <ul className="charFilmsList">{filmData}</ul>
       </ul>
--- server/controllers/starWarsController.js
+++ server/controllers/starWarsController.js
@@ -1,11 +1,19 @@
-const models = require('../models/starWarsModels');
+/* eslint-disable quotes */
+const models = require("../models/starWarsModels");
+
+// const { Person } = require("../models/starWarsModels");
 
 const starWarsController = {};
 
-starWarsController.getCharacters = (req, res, next) => {
-  // write code here
-
-  next();
+starWarsController.getCharacters = async (req, res, next) => {
+  try {
+    const characters = await models.Person.find({});
+    console.log(characters);
+    res.locals = characters;
+    // res.send(characterArray);
+  } catch (err) {
+    next(err);
+  }
 };
 
 starWarsController.getSpecies = (req, res, next) => {
